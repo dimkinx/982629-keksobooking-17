228fa7778cb5b2301e48c6ce8057255c290c6784
Дмитрий <htmlacademy.ostroukhov@gmail.com>
2019-07-14 19:12:47 +0300

    Выносит константы в отдельный модуль

---
+++ js/constants.js
@@ -0,0 +1,24 @@
+'use strict';
+
+(function () {
+  var OFFERS_NUM = 8;
+
+  var PinSize = {
+    WIDTH: 50,
+    HEIGHT: 70,
+    RADIUS: 25,
+  };
+
+  var MainPinSize = {
+    WIDTH: 65,
+    HEIGHT: 65,
+    HEIGHT_WITH_POINTER: 80,
+    RADIUS: 33,
+  };
+
+  window.constants = {
+    OFFERS_NUM: OFFERS_NUM,
+    PinSize: PinSize,
+    MainPinSize: MainPinSize,
+  };
+})();
